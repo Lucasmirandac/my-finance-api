445602f4d68d48838eaa2ff31313c8037457696a
Lucas Costa <lucas.costa@atletico.com.br>
2024-03-25 15:17:37 -0300

    :wrench: configuring module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,33 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { BudgetModule } from './budget/budget.module';
+import { TransactionModule } from './transaction/transaction.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user/entities/user.entity';
+import { Transaction } from './transaction/transaction.entity';
+import { Budget } from './budget/budget.entity';
+import { UserTag } from './user/entities/user-tag.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    UserModule,
+    BudgetModule,
+    TransactionModule,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'my_finance',
+      entities: [User, Transaction, Budget, UserTag],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
